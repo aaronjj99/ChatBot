fb2dd94e0e8d4b7da7f219bb0c12ebe57d95c01e
axelnorth <yo.com32@gmail.com>
2020-11-07 21:57:42 +0200

    no message

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$link = mysqli_connect ('localhost', 'root', '', 'bot_quest');
-
-if (mysqli_connect_errno ())
-{
-    echo 'ERROR ('.mysqli_connect_errno().'):'.mysqli_connect_error();
-    exit();
-}
